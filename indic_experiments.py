41d49f5b9d9779fb4465617a751da66dec598862
Clifford Ressel <csressel@gmail.com>
2018-11-28 10:17:00 -0500

    cloze tasks woohoo

--- indic_experiments.py
+++ indic_experiments.py
@@ -1,9 +1,17 @@
 
+from functools import reduce
+from itertools import combinations
+import random
+
 import numpy as np
 from scipy.stats import zscore
 from sklearn.model_selection import KFold
+
 from vowel_typology_model import VowelTypologyModel
 from indic_formant_data import IndicFormantData
+
+K = 10
+CLOZE_TRIALS = 1000
 
 model = VowelTypologyModel()
 formant_data = IndicFormantData()
@@ -24,7 +32,14 @@ for i, (sample, label) in enumerate(zip(reversed(aggr_data), reversed(aggr_labs)
 # delete the third value (f3) along axis=2
 aggr_data = np.delete(aggr_data, 2, axis=2)
 zsc_data = zscore(aggr_data, axis=1)
-kf = KFold(n_splits=10)
+
+# reproducable results
+random.seed(1300031)
+np.random.seed(1300031)
+np.random.shuffle(zsc_data)
+
+def e(v):
+    return v
 
 # average per-vowel measurements (using middle four intervals), returning an
 # inventory with one entry per vowel
@@ -59,43 +74,160 @@ def preprocess(samples, labels):
 
     return vowel_inv
 
-def e(v):
-    return v
-
+# ------------------------------------------------------------------------------
 # 10 fold evaluation of probability of the fold's validation set's assigned
 # probability mass
+
+kf = KFold(n_splits=K)
+
 bestbppresult = float("inf")
 bestmppresult = float("inf")
 print()
+print(f'Running {K} folds of estimating fold\'s validation set\'s probability mass')
+
 for i, (train_idx, val_idx) in enumerate(kf.split(zsc_data, aggr_labs)):
-    print("fold " + str(i+1) + "\tstarting at " + str(val_idx[0]) + "\twith probability = ", end='')
+    print(f'fold {str(i+1)}\tstarting at {str(val_idx[0])}\twith probability = ', end='')
     val_x = zsc_data[val_idx]
     val_y = aggr_labs[val_idx]
     vowel_inv = preprocess(val_x, val_y)
 
-    def e(v):
-        return v
     bppresult = model.bpp(inv=vowel_inv, embeddings=e)
     mppresult = model.mpp(inv=vowel_inv, embeddings=e)
-    print(str(int(bppresult)) + " (bpp) / ", end='')
-    print(str(int(mppresult)) + " (mpp)")
+    print(f'{str(int(bppresult))} (bpp) / ', end='')
+    print(f'{str(int(mppresult))} (mpp)')
     if bppresult < bestbppresult:
         bestbppresult = bppresult
     if mppresult < bestmppresult:
         bestmppresult = mppresult
 
+# NOTES
 # essentially zero probability, because
 #   A) the model doesn't do focalization correctly
 #   B) these vowel inventories are real big, even in the KFold
 # need to fix the VowelTypologyModel, and consider removing the least common
 # vowels from this dataset?
-print("\tBPP assigns probability = " + str(bestbppresult))
-print("\tMPP assigns probability = " + str(bestmppresult))
 
-# cloze tasks
-#   through model inference
-#   through neural net
+print(f'\tBPP assigns probability = {str(bestbppresult)}')
+print(f'\tMPP assigns probability = {str(bestmppresult)}')
 
+# ------------------------------------------------------------------------------
+# 100 trials each of cloze task on 1 vowel removed, 0 or 1 vowels removed,
+# and 0 or 1 or 2 vowels removed (all through model inference)
+
+total_vowel_inv = preprocess(val_x, val_y)
+inv = {}
+for fs, v in total_vowel_inv:
+    inv[v] = fs
+bpp1s = []
+mpp1s = []
+bpp01s = []
+mpp01s = []
+bpp012s = []
+mpp012s = []
+bpp1guesses = []
+
+print()
+print(f'Running {CLOZE_TRIALS} trials of cloze-1, cloze-01, cloze-012 tasks')
+for i in range(CLOZE_TRIALS):
+
+    def infer(candidates, answer, bppresults, mppresults):
+        bppbestconf, bppbestpred = float("-inf"), None
+        mppbestconf, mppbestpred = float("-inf"), None
+
+        for candidate, guess in candidates:
+            bppconf = model.bpp(candidate, e)
+            mppconf = model.mpp(candidate, e)
+            if bppconf > bppbestconf:
+                bppbestconf = bppconf
+                bppbestpred = guess
+            if mppconf > mppbestconf:
+                mppbestconf = mppconf
+                mppbestpred = guess
+
+        bppresults.append(int(answer == bppbestpred))
+        mppresults.append(int(answer == mppbestpred))
+
+    # --------------------------------------------------------------------------
+    # cloze-1
+
+    # copy so as not to delete from total inv
+    cloze1_inv = list(total_vowel_inv)
+    candidates = []
+    i = random.randint(0, len(cloze1_inv) - 1)
+    answer = set([vowel_inv[i][1]])
+    del cloze1_inv[i]
+    vowels = [v for fs,v in cloze1_inv]
+
+    for v in inv.keys():
+        if v not in vowels:
+            candidates.append((cloze1_inv + [(inv[v], v)], set([v])))
+
+    infer(candidates, answer, bpp1s, mpp1s)
+
+    # --------------------------------------------------------------------------
+    # cloze-01
+
+    cloze01_inv = list(total_vowel_inv)
+    candidates = []
+    del_count = random.randint(0, 1)
+    answer = set()
+    for i in range(del_count):
+        i = random.randint(0, len(cloze01_inv) - 1)
+        answer.add(vowel_inv[i][1])
+        del cloze01_inv[i]
+    vowels = [v for fs,v in cloze01_inv]
+
+    for v in inv.keys():
+        if v not in vowels:
+            candidates.append((cloze01_inv + [(inv[v], v)], set([v])))
+    candidates.append((cloze01_inv, set([])))
+
+    infer(candidates, answer, bpp01s, mpp01s)
+
+    # --------------------------------------------------------------------------
+    # cloze-012
+
+    cloze012_inv = list(total_vowel_inv)
+    candidates = []
+    del_count = random.randint(0, 2)
+    answer = set()
+    for i in range(del_count):
+        i = random.randint(0, len(cloze012_inv) - 1)
+        answer.add(vowel_inv[i][1])
+        del cloze012_inv[i]
+    vowels = [v for fs,v in cloze012_inv]
+
+    candidates.append((cloze012_inv, set([])))
+    for v in inv.keys():
+        if v not in vowels:
+            candidates.append((cloze012_inv + [(inv[v], v)], set([v])))
+    for v1, v2 in combinations(inv.keys(), 2):
+        if v1 not in vowels and v2 not in vowels:
+            candidates.append((cloze012_inv + [(inv[v], v)], set([v1, v2])))
+
+    infer(candidates, answer, bpp012s, mpp012s)
+
+# NOTES
+# this is still pretty meaningless, partly because with the aggregated
+# inventory, there's not tough choice of phoneme (if there's one removed,
+# there's usually only one left in the inv to choose from).
+# so candidates should also have the becker corpus vowels added? or create
+# indic/becker equivalence and then add candidates between the union of the two
+# sets.
+# or lastly, include instances of indic data's inventories in the training of
+# the vowel typology model to make sure these inventories have greater
+# probability mass.
+
+print(f'\tBPP on cloze1 scores acc = {sum(bpp1s) / CLOZE_TRIALS * 100}%')
+print(f'\tBPP on cloze01 scores acc = {sum(bpp01s) / CLOZE_TRIALS * 100}%')
+print(f'\tBPP on cloze012 scores acc = {sum(bpp012s) / CLOZE_TRIALS * 100}%')
+print(f'\tMPP on cloze1 scores acc = {sum(mpp1s) / CLOZE_TRIALS * 100}%')
+print(f'\tMPP on cloze01 scores acc = {sum(mpp01s) / CLOZE_TRIALS * 100}%')
+print(f'\tMPP on cloze012 scores acc = {sum(mpp012s) / CLOZE_TRIALS * 100}%')
+
+# TODO ?
+# change the previous two evaluation tasks to be _voice_ specific instead of
+#   aggregated
 # classification tasks (vowel, speaker, dialect)
 #   through model inference
 #   through neural net
